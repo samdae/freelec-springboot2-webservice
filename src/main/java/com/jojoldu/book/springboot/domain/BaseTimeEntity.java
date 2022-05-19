01f1b2151fca451fc8bf42ceca40d883b5e77bd0
이대훈 <samdae3@naver.com>
2022-05-19 14:53:44 +0900

    BaseTimeEntity 오타 수정 / gradle, properties 정리

--- src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
@@ -5,6 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -26,7 +27,7 @@ public class BaseTimeEntity {
      * Entity 가 생성되어 저장될 때 시간이 자동 저장됨
      */
     @CreatedDate
-    private LocalDateTime createDate;
+    private LocalDateTime createdDate;
 
     /**
      * 조회한 Entity 의 값을 변경할 때 시간이 자동 저장 됨
