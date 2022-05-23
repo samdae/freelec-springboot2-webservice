3c53206f099ddb607d8463c1791634510e8b20d2
이대훈 <samdae3@naver.com>
2022-05-23 11:13:59 +0900

    무중단 배포 구축 시작 3(deploy.sh 수정)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.jar $PROJECT_NAME
+cp $REPOSITORY/zip/*.jar $REPOSITORY
 
 ehco "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
