405ca6eea5b32ffc23e6e93c5418eb33ef174186
이대훈 <samdae3@naver.com>
2022-05-23 14:41:10 +0900

    무중단 배포 구축 시작 9 - API 404 ? local에서는 정상, 서버엔 404

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -2,16 +2,13 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
-import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
-import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.servlet.http.HttpSession;
 
--- src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -14,7 +15,7 @@ public class ProfileController {
     private final Environment env;
 
     @GetMapping("/profile")
-    public String profile() {
+    public @ResponseBody String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
 
